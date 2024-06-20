63e51ced75f7d76b6b7e066fed1a92f03dd6f391
mikeyang <18604713262@163.com>
2024-06-19 20:26:57 -0400

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import {resolve} from "path";
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
 
   router: {
     base: '/webapp/'
