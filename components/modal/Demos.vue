af9a1a9bcf9688de67cb14b2ab1a8b9fd5f25d95
mikeyang <18604713262@163.com>
2024-06-20 16:04:58 -0400

    Pinia SSR problems fix.

--- components/modal/Demos.vue
+++ components/modal/Demos.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-const isOpen = ref(false);
+import { ref } from 'vue'
+import { useCounterStore } from '@/store/myStore'
+
+const isOpen = ref(false)
 const items = [
   {
     label: 'Getting Started',
     icon: 'i-heroicons-information-circle',
     defaultOpen: true,
-    disabled:false,
+    disabled: false,
     slot: 'getting-started',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
   },
@@ -13,13 +16,10 @@ const items = [
     label: "Pinia Store",
     icon: 'i-material-symbols-storage',
     slot: "pinia-store",
-
   }
+]
 
-];
-
-const counterStore = useCounterStore();
-
+const counterStore = useCounterStore()
 </script>
 
 <template>
@@ -28,27 +28,28 @@ const counterStore = useCounterStore();
 
     <UModal v-model="isOpen">
       <div class="p-4">
-          <UAccordion :items="items" >
-            <template #item="{ item }">
-              <p class="italic text-gray-900 dark:text-white text-center">
-                {{ item.content }}
-              </p>
-            </template>
+        <UAccordion :items="items">
+          <template #item="{ item }">
+            <p class="italic text-gray-900 dark:text-white text-center">
+              {{ item.content }}
+            </p>
+          </template>
 
-            <template #getting-started>
-              <h2 class="text-lg font-bold">Getting Started</h2>
-              <p class="text-sm text-gray-500">Learn how to use this app</p>
-            </template>
+          <template #getting-started>
+            <h2 class="text-lg font-bold">Getting Started</h2>
+            <p class="text-sm text-gray-500">Learn how to use this app</p>
+          </template>
 
-            <template #pinia-store >
-              <h2 class="text-lg font-bold">Pinia Store Counter Demo</h2>
-              <p class="text-sm text-gray-500">{{counterStore}}</p>
+          <template #pinia-store>
+            <h2 class="text-lg font-bold">Pinia Store Counter Demo</h2>
+            <p class="text-sm text-gray-500">Count: {{ counterStore.count }}</p>
+            <p class="text-sm text-gray-500">Double Count: {{ counterStore.doubleCount }}</p>
+            <p class="text-sm text-gray-500">Name: {{ counterStore.name }}</p>
 
-              <UButton @click="counterStore.increment" :ui="{ rounded: 'rounded-full' }">+</UButton>
-              <UButton @click="counterStore.decrement" :ui="{ rounded: 'rounded-full' }">-</UButton>
-
-            </template>
-          </UAccordion>
+            <UButton @click="counterStore.increment" :ui="{ rounded: 'rounded-full' }">+</UButton>
+            <UButton @click="counterStore.decrement" :ui="{ rounded: 'rounded-full' }">-</UButton>
+          </template>
+        </UAccordion>
       </div>
     </UModal>
   </div>
