8061ea30a8412dd40b264e20030351a10c3165b1
mikeyang <18604713262@163.com>
2024-07-05 19:55:41 -0400

    Icons stuff

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -3,7 +3,7 @@
 
     <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar"
             aria-controls="default-sidebar" type="button"
-            class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg
+            class="inline-flex flex-row items-center gap-4 p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg
             sm:hidden
             hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200
             dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -19,54 +19,60 @@
            aria-label="Sidebar">
       <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
-          <li>
-            <NuxtLink class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+          <li class="w-full">
+            <NuxtLink class="flex w-full items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 
-              <IconsAccount />
+              <LazyIconsAccount />
             </NuxtLink>
           </li>
-          <li>
-            <NuxtLink  to="/webapp/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+          <li class="w-full">
+            <NuxtLink  to="/webapp/"
+                       class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 
-              Home
-            </NuxtLink>
-          </li>
-
-
-          <li>
-            <NuxtLink to="/webapp/notes" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <IconsNoteDefault/> Notes
-            </NuxtLink>
-          </li>
-
-          <li>
-            <NuxtLink to="/webapp/chat" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-             <IconsChatDefault/> Chat
-            </NuxtLink>
-          </li>
-
-          <li>
-            <NuxtLink to="/webapp/tools" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <IconsAppGrok/> Tools
+              <LazyIconsSubwayHome/> Home
             </NuxtLink>
           </li>
 
 
-          <li>
-            <NuxtLink to="/webapp/user" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <IconsUserCog/> User
+          <li class="w-full">
+            <NuxtLink to="/webapp/notes"
+                      class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <LazyIconsNoteDefault/> Notes
             </NuxtLink>
           </li>
 
-          <li>
+          <li class="w-full">
+            <NuxtLink to="/webapp/chat"
+                      class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+             <LazyIconsChatDefault/> Chat
+            </NuxtLink>
+          </li>
+
+          <li class="w-full">
+            <NuxtLink to="/webapp/tools"
+                      class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <LazyIconsSubwayKey/> Tools
+            </NuxtLink>
+          </li>
+
+
+          <li class="w-full">
+            <NuxtLink to="/webapp/user"
+                      class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <LazyIconsAccount /> User
+            </NuxtLink>
+          </li>
+
+          <li class="w-full">
             <NuxtLink to="/webapp/settings"
-                      class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <IconsCog/> Settings
+                      class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <LazyIconsCog/> Settings
             </NuxtLink>
           </li>
 
-          <li>
-            <NuxtLink to="/webapp/about" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+          <li class="w-full">
+            <NuxtLink to="/webapp/about"
+                      class="flex flex-row items-center gap-4 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
               About
             </NuxtLink>
           </li>
