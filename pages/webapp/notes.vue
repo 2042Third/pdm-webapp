e28b7d39d1d0f240a014e3b4af00e96d8b8500ff
mikeyang <18604713262@163.com>
2024-07-06 04:04:43 -0400

    Bug fix

--- pages/webapp/notes.vue
+++ pages/webapp/notes.vue
@@ -1,12 +1,14 @@
 <template>
+  <div class="" >
   <NoteSideBar>
-    <div class="flex flex-grow gap-6 mb-6 justify-center items-center ">
+    <div class="flex gap-6 mb-6 justify-center items-center ">
       <CommonContainerDotted containerClass="max-w-prose w-full h-full " innerClass="flex flex-col gap-4">
         <UTextarea
         />
       </CommonContainerDotted>
     </div>
   </NoteSideBar>
+  </div>
 </template>
 
 <script setup>
