b4502e239ef8e393f16c5db8c1e3ef12ad0d21dc
mikeyang <18604713262@163.com>
2024-11-27 19:20:52 -0500

    Webapp

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,9 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'pdm-webapp',
-    script: '.output/server/index.mjs',
-    env: {
-      NODE_ENV: 'production',
+  apps: [
+    {
+      name: 'pdm-webapp',
+      script: '.output/server/index.mjs',
+      env: {
+        NODE_ENV: 'production',
+      },
+      out_file: '/home/Static8148/webapp/logs/pdm-webapp-out.log', // stdout log
+      error_file: '/home/Static8148/webapp/logs/pdm-webapp-error.log', // stderr log
+      log_file: '/home/Static8148/webapp/logs/pdm-webapp-combined.log', // combined log
+      merge_logs: true // Ensures better organization by combining logs
     }
-  }]
-}
+  ]
+};
